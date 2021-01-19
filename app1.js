b204c4bee06ecd34645f2d2349e161a796d563e4
PinzaruAndreea <66926634+PinzaruAndreea@users.noreply.github.com>
2021-01-19 22:52:52 +0200

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,12 +1,17 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
-});
+//set port
+const port = process.env.PORT || 8080
 
-server.listen(port,() => {
-    console.log(`Server running at port `+port);
-});
+app.use(express.static(__dirname));
+
+//routes
+
+app.get("/", function(req, res) {
+    res.render("./index.html");
+})
+
+app.listen(port, function() {
+    console.log("app running");
+})
